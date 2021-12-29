932115561f54496f97fe12f064acdd2fd31fb88c
tsedveet <tsedvee.tt@gmail.com>
2021-12-29 17:20:52 +0800

    bootstrap version

---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Nav,
+  Media,
+  Image,
+  Card,
+} from "react-bootstrap";
+
+import MyNavbar from "components/my-navbar";
+import Intro from "components/intro";
+import ListItem from "components/list-item";
+import GridItem from "components/grid-item";
+
+export default function Home() {
+  return (
+    <Container>
+      <MyNavbar />
+      <div className="blog-detail-page">
+        <Row>
+          <Col md="12">
+            <Intro />
+          </Col>
+        </Row>
+
+        <hr />
+
+        <div className={`page-wrapper`}>
+          <Row className="mb-5">
+            <Col md="10">
+              <ListItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+          </Row>
+        </div>
+      </div>
+      <footer className="page-footer">
+        <div>
+          <a href="#">нүүр</a>
+          {" | "}
+          <a href="#">сургалт</a>
+          {" | "}
+          <a href="#">фэйсбүүк</a>
+        </div>
+      </footer>
+    </Container>
+  );
+}
